15cbd1d855f6e4f3fa12705dfca210fc42348b36
DubblePos <88361186+DubblePos@users.noreply.github.com>
2021-11-16 17:29:50 +0900

    2021/11/16 회귀분석
    회귀분석

---
+++ Ch09/9_2_워드클라우드 실습.R
@@ -0,0 +1,13 @@
+# 날짜 : 2021/09/30
+# 이름 : 박승필
+# 내용 : 정형과 비정형 데이터 처리 - 텍스트 시각화 실습 교재 p301
+
+install.packages('https://ftp.harukasan.org/CRAN/bin/windows/contrib/3.4/KoNLP_0.80.1.zip', repos = NULL)
+install.packages('Sejong')
+install.packages('hash')
+install.packages('rJava')
+install.packages('tau')
+install.packages('RSQLite')
+install.packages('devtools')
+install.packages('https://ftp.harukasan.org/CRAN/bin/windows/contrib/3.6/devtools_2.4.1.zip')
+library(KoNLP)
